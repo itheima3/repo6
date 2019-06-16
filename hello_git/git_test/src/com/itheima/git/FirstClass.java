ec0177d989d57f38baa668ef9df4e0d36764731a
duanxianbao <1305132624@qq.com>
2019-06-16 22:18:32 +0800

    修改FirstCalss

--- hello_git/git_test/src/com/itheima/git/FirstClass.java
+++ hello_git/git_test/src/com/itheima/git/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,git");
+        //hello
     }
 
     public void test(){
